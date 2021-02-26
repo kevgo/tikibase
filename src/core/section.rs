c2b4640c7178a19143a6028f69d75a531af372c6
Kevin Goslar <kevin.goslar@gmail.com>
2021-02-26 14:16:00 -0600

    stats: show section type counts

--- src/core/section.rs
+++ src/core/section.rs
@@ -7,6 +7,43 @@ pub struct Section {
   pub body: Vec<Line>,
 }
 
+impl Section {
+  pub fn section_type(&self) -> String {
+    let pos = self
+      .title_line
+      .char_indices()
+      .find(|p| p.1 != '#' && p.1 != ' ');
+    match pos {
+      None => "".to_string(),
+      Some(pos) => self.title_line.clone().split_off(pos.0),
+    }
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn section_type() {
+    let tests = vec![
+      ("# Title", "Title"),
+      ("### Title", "Title"),
+      ("Title", "Title"),
+      ("###", ""),
+    ];
+    for (give, want) in tests.into_iter() {
+      let section = Section {
+        line_number: 2,
+        title_line: give.to_string(),
+        body: vec![],
+      };
+      let have = section.section_type();
+      assert_eq!(have, want.to_string(), "want: '{}', have: '{}'", want, have);
+    }
+  }
+}
+
 pub struct Line {
   /// The line number relative to the section title line, 0-based.
   pub line_number: u32,
--- src/stats/mod.rs
+++ src/stats/mod.rs
@@ -6,6 +6,7 @@ pub fn run() {
   println!("documents: {}", tb.docs.len());
   println!("resources: {}", tb.resources.len());
   let section_types = collect_section_types(&tb);
+  println!();
   println!("{} section types:", section_types.len());
   for (name, count) in section_types {
     println!("- {} ({})", name, count);
@@ -16,14 +17,12 @@ fn collect_section_types(tb: &tikibase::Tikibase) -> HashMap<String, u32> {
   let mut result: HashMap<String, u32> = HashMap::new();
   for doc in &tb.docs {
     for section in &doc.content_sections {
-      let title_line = result.get(&section.title_line);
-      match title_line {
-        None => {
-          result.insert(section.title_line.clone(), 1);
-        }
+      let section_type = section.section_type();
+      match result.get(&section_type) {
+        None => result.insert(section_type, 1),
         Some(count) => {
           let new = count + 1;
-          result.insert(section.title_line.clone(), new);
+          result.insert(section_type, new)
         }
       };
     }
