9423e8575615798047e9768a2ebd71f62937ff53
Kevin Goslar <kevin.goslar@gmail.com>
2021-03-02 22:17:23 -0600

    Remove warning in Cucumber code

--- tests/cucumber.rs
+++ tests/cucumber.rs
@@ -6,14 +6,12 @@ use cucumber_rust::{async_trait, given, then, when, World, WorldInit};
 pub struct MyWorld {
   // You can use this struct for mutable context in scenarios.
   foo: String,
-  bar: usize,
   some_value: RefCell<u8>,
 }
 
 impl MyWorld {
   async fn test_async_fn(&mut self) {
     *self.some_value.borrow_mut() = 123u8;
-    self.bar = 123;
   }
 }
 
@@ -24,7 +22,6 @@ impl World for MyWorld {
   async fn new() -> Result<Self, Infallible> {
     Ok(Self {
       foo: "wat".into(),
-      bar: 0,
       some_value: RefCell::new(0),
     })
   }
