b2d088cf3560293ad65042ca40fe2843496478c7
Kevin Goslar <kevin.goslar@gmail.com>
2021-04-04 14:22:41 -0500

    Remove all errors and warnings

--- src/core/link.rs
+++ src/core/link.rs
@@ -1,4 +1,4 @@
-use heck::KebabCase;
+// use heck::KebabCase;
 
 /// a link on a line
 #[derive(Debug, PartialEq)]
@@ -8,26 +8,26 @@ pub struct Link {
 }
 
 impl Link {
-    /// provides the local anchor for this link
-    pub fn local_anchor(&self) -> String {
-        Link::to_anchor(&self.title)
-    }
+    // /// provides the local anchor for this link
+    // pub fn local_anchor(&self) -> String {
+    //     Link::to_anchor(&self.title)
+    // }
 
-    /// converts the given link title into a GitHub-compatible link anchor
-    fn to_anchor(title: &str) -> String {
-        format!("#{}", title.to_kebab_case())
-    }
+    // /// converts the given link title into a GitHub-compatible link anchor
+    // fn to_anchor(title: &str) -> String {
+    //     format!("#{}", title.to_kebab_case())
+    // }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+    // use super::*;
 
-    #[test]
-    fn to_anchor() {
-        let tests = vec![("foo", "#foo")];
-        for (give, want) in tests.into_iter() {
-            assert_eq!(Link::to_anchor(give), want);
-        }
-    }
+    // #[test]
+    // fn to_anchor() {
+    //     let tests = vec![("foo", "#foo")];
+    //     for (give, want) in tests.into_iter() {
+    //         assert_eq!(Link::to_anchor(give), want);
+    //     }
+    // }
 }
