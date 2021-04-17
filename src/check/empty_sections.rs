33238bbccccd34d2cf7b6bea54ae11f2d49f289a
Kevin Goslar <kevin.goslar@gmail.com>
2021-04-17 16:54:14 -0500

    progress

--- src/check/empty_sections.rs
+++ src/check/empty_sections.rs
@@ -29,33 +29,32 @@ pub fn process(base: &mut Tikibase, fix: bool) -> Vec<String> {
 #[cfg(test)]
 mod tests {
 
-    #[cfg(test)]
-    mod find {
+    use super::process;
+    use crate::core::document::Document;
+    use crate::core::tikibase::Tikibase;
 
-        use crate::core::tikibase::Tikibase;
-
-        #[test]
-        fn section_without_body_lines() {
-            let content = "\
+    #[test]
+    fn false_empty_section() {
+        let content = "\
 # test document
 
 ### empty section
 ### next section
 
 content";
-            let doc = crate::core::document::Document::from_str(content, "test.md");
-            let mut base = Tikibase::with_doc(doc);
-            let have = super::super::process(&mut base, false);
-            assert_eq!(have.len(), 1);
-            assert_eq!(
-                have[0],
-                "test.md:3  section \"empty section\" has no content"
-            );
-        }
+        let doc = Document::from_str(content, "test.md");
+        let mut base = Tikibase::with_doc(doc);
+        let have = process(&mut base, false);
+        assert_eq!(have.len(), 1);
+        assert_eq!(
+            have[0],
+            "test.md:3  section \"empty section\" has no content"
+        );
+    }
 
-        #[test]
-        fn section_with_empty_body_line() {
-            let content = "\
+    #[test]
+    fn false_empty_line() {
+        let content = "\
 # test document
 
 ### empty section
@@ -63,28 +62,27 @@ content";
 ### next section
 
 content";
-            let doc = crate::core::document::Document::from_str(content, "test.md");
-            let mut base = Tikibase::with_doc(doc);
-            let have = super::super::process(&mut base, false);
-            assert_eq!(have.len(), 1);
-            assert_eq!(
-                have[0],
-                "test.md:3  section \"empty section\" has no content"
-            );
-        }
+        let doc = Document::from_str(content, "test.md");
+        let mut base = Tikibase::with_doc(doc);
+        let have = process(&mut base, false);
+        assert_eq!(have.len(), 1);
+        assert_eq!(
+            have[0],
+            "test.md:3  section \"empty section\" has no content"
+        );
+    }
 
-        #[test]
-        fn section_with_content() {
-            let content = "\
+    #[test]
+    fn false_content() {
+        let content = "\
 # test document
 
 ### section with content
 
 content";
-            let doc = crate::core::document::Document::from_str(content, "test.md");
-            let mut base = Tikibase::with_doc(doc);
-            let have = super::super::process(&mut base, false);
-            assert_eq!(have.len(), 0);
-        }
+        let doc = Document::from_str(content, "test.md");
+        let mut base = Tikibase::with_doc(doc);
+        let have = process(&mut base, false);
+        assert_eq!(have.len(), 0);
     }
 }
