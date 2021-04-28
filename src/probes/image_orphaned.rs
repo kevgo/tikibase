a9dd48d3e86af52e5e2f63749966c9e723a30f86
Kevin Goslar <kevin.goslar@gmail.com>
2021-04-27 20:31:16 -0500

    progress

--- src/probes/image_orphaned.rs
+++ src/probes/image_orphaned.rs
@@ -5,7 +5,7 @@ pub fn process(base: &Tikibase, resource_links: Vec<String>) -> Result {
     let mut result = Result::new();
     for resource in base.resources.iter() {
         let path = &resource.path.to_string_lossy().to_string();
-        if !resource_links.contains(path) {
+        if !resource_links.contains(&path) {
             result.findings.push(format!("unused image \"{}\"", path));
         }
     }
