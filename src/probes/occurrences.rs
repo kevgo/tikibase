99a30fdac6025d0485fbfe48b309be7c11aea9c2
Kevin Goslar <kevin.goslar@gmail.com>
2021-05-01 07:49:28 -0500

    progress

---
+++ src/probes/occurrences.rs
@@ -0,0 +1,9 @@
+pub fn process(base: &mut Tikibase, fix: bool) -> Outcome {
+    // determine all links to this document
+
+    // determine all links in this document
+
+    // determine missing links in this document
+
+    // optionally add occurrences section
+}
