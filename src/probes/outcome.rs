14a780cc9cd73f6057f743aea9a031d8a0e94ae2
Kevin Goslar <kevin.goslar@gmail.com>
2021-05-12 11:42:41 -0500

    progress

--- src/probes/outcome.rs
+++ src/probes/outcome.rs
@@ -2,14 +2,14 @@ use crate::core::tikibase::Tikibase;
 
 /// an issue that was identified in the Tikibase
 pub trait Issue {
+    /// provides a human-readable description of the issue
+    fn describe(&self) -> String;
+
     /// fixes this issue, returns a description of what it did
     fn fix(&self, base: &mut Tikibase) -> String;
 
     /// indicates whether this issues is fixable
     fn fixable(&self) -> bool;
-
-    /// provides a human-readable description of the issue
-    fn describe(&self) -> String;
 }
 
 /// a sorted list of issues
@@ -21,12 +21,13 @@ impl Issues {
         self.0.append(&mut new_issues);
     }
 
-    /// provides an empty issue list
-    pub fn new() -> Issues {
-        Issues(vec![])
-    }
-
+    /// provides the issues stored in this custom class
     pub fn issues(self) -> Vec<Box<dyn Issue>> {
         self.0
+    }
+
+    /// provides an empty Issues instance
+    pub fn new() -> Issues {
+        Issues(vec![])
     }
 }
