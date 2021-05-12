ec3d5d6aa98af96cc32aedd34b2fc59cf637bf13
Kevin Goslar <kevin.goslar@gmail.com>
2021-05-11 22:12:28 -0500

    progress

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,68 @@ pub mod help;
 pub mod probes;
 pub mod stats;
 pub mod testhelpers;
+
+use crate::core::tikibase::Tikibase;
+use std::path::PathBuf;
+
+#[derive(Debug, PartialEq)]
+pub enum Command {
+    Check,
+    Fix,
+    Help,
+    Pitstop,
+    Stats,
+    Version,
+}
+
+pub fn process(command: Command, path: PathBuf) -> Vec<String> {
+    let mut result = Vec::new();
+    // step 1: load the Tikibase
+    let (mut base, mut errors) = Tikibase::load(path);
+    result.append(&mut errors);
+
+    // step 2: basic command --> execute and exit
+    let mut basic_command = true;
+    match command {
+        Command::Help => help::run(),
+        Command::Stats => stats::run(&base),
+        Command::Version => help::version(),
+        _ => basic_command = false,
+    }
+    if basic_command {
+        return result;
+    }
+
+    // here we have a complex command
+
+    // step 3: find the issues in the Tikibase
+    let issues = probes::run(&base);
+
+    // step 4: take care of the issues
+    let mut outcomes = match command {
+        Command::Check => issues
+            .into_iter()
+            .map(|issue| issue.describe())
+            .collect::<Vec<String>>(),
+        Command::Fix => issues
+            .into_iter()
+            .filter(|issue| issue.fixable())
+            .map(|issue| issue.fix(&mut base))
+            .collect::<Vec<String>>(),
+        Command::Pitstop => issues
+            .into_iter()
+            .map(|issue| {
+                if issue.fixable() {
+                    issue.fix(&mut base)
+                } else {
+                    issue.describe()
+                }
+            })
+            .collect::<Vec<String>>(),
+        _ => {
+            panic!("unexpected complex command: {:?}", command);
+        }
+    };
+    result.append(&mut outcomes);
+    result
+}
--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,16 @@
 extern crate lazy_static;
 
 use std::path::PathBuf;
-use tikibase::core::tikibase::Tikibase;
-use tikibase::help;
-use tikibase::probes;
-use tikibase::stats;
+
+use tikibase::process;
+use tikibase::Command;
 
 fn main() {
-    // step 1: determine the configuration
-    let command = parse(std::env::args());
-
-    // step 2: load the Tikibase
-    let (mut base, errors) = Tikibase::load(PathBuf::from("."));
-    for error in errors {
-        println!("{}", error);
-    }
-
-    // step 3: execute basic commands
-    let mut basic_command = true;
-    match command {
-        Command::Help => help::run(),
-        Command::Stats => stats::run(&base),
-        Command::Version => help::version(),
-        _ => basic_command = false,
-    }
-    if basic_command {
-        return;
-    }
-
-    // step 4: find the issues in the Tikibase
-    let issues = probes::run(&base);
-
-    // step 5: take care of the issues
-    let mut outcomes = match command {
-        Command::Check => issues
-            .into_iter()
-            .map(|issue| issue.describe())
-            .collect::<Vec<String>>(),
-        Command::Fix => issues
-            .into_iter()
-            .filter(|issue| issue.fixable())
-            .map(|issue| issue.fix(&mut base))
-            .collect::<Vec<String>>(),
-        Command::Pitstop => issues
-            .into_iter()
-            .map(|issue| {
-                if issue.fixable() {
-                    issue.fix(&mut base)
-                } else {
-                    issue.describe()
-                }
-            })
-            .collect::<Vec<String>>(),
-        _ => {
-            panic!("unexpected complex command: {:?}", command);
-        }
-    };
-
-    // step 6: print the results
+    let mut outcomes = process(parse(std::env::args()), PathBuf::from("."));
     outcomes.sort();
     for outcome in outcomes {
         println!("{}", outcome);
     }
-}
-
-#[derive(Debug, PartialEq)]
-enum Command {
-    Check,
-    Fix,
-    Help,
-    Pitstop,
-    Stats,
-    Version,
 }
 
 /// Provides the command-line arguments as a Rust struct.
@@ -95,7 +34,7 @@ where
 
 #[cfg(test)]
 mod tests {
-    use super::Command::{Check, Help, Stats, Version};
+    use tikibase::Command::{Check, Help, Stats, Version};
 
     #[test]
     fn parse() {
