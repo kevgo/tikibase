dbf93adacac1f25cbed204e2d68f15f9618b1094
Kevin Goslar <kevin.goslar@gmail.com>
2021-05-11 20:32:36 -0500

    progress

--- src/probes/image_orphaned.rs
+++ src/probes/image_orphaned.rs
@@ -1,13 +1,31 @@
-use super::outcome::Outcome;
+use super::outcome::Issue;
 use super::Tikibase;
 
-pub fn process(base: &Tikibase, resource_links: Vec<String>) -> Outcome {
-    let mut result = Outcome::new();
+pub fn process(base: &Tikibase, resource_links: Vec<String>) -> Vec<Box<dyn Issue>> {
+    let mut result = Vec::<Box<dyn Issue>>::new();
     for resource in base.resources.iter() {
-        let path = &resource.path.to_string_lossy().to_string();
+        let path = resource.path.to_string_lossy().to_string();
         if !resource_links.contains(&path) {
-            result.findings.push(format!("unused image \"{}\"", path));
+            result.push(Box::new(OrphanedImage { path }));
         }
     }
     result
+}
+
+pub struct OrphanedImage {
+    path: String,
+}
+
+impl Issue for OrphanedImage {
+    fn fix(self, base: &mut Tikibase) -> String {
+        panic!("not fixable")
+    }
+
+    fn fixable(&self) -> bool {
+        false
+    }
+
+    fn describe(self) -> String {
+        format!("unused image \"{}\"", self.path)
+    }
 }
--- src/probes/mod.rs
+++ src/probes/mod.rs
@@ -15,17 +15,17 @@ pub fn run(base: &Tikibase) -> Vec<Box<dyn outcome::Issue>> {
     issues.append(section_capitalization::process(&base));
     let mut links_result = link_broken::process(&base);
     issues.append(links_result.issues);
-    issues.append(&mut image_orphaned::process(
+    issues.append(image_orphaned::process(
         &base,
         links_result.outgoing_resource_links,
     ));
-    // let mut occ_res = occurrences::process(
-    //     base,
-    //     links_result.incoming_doc_links,
-    //     links_result.outgoing_doc_links,
-    //     fix,
-    // );
-    // issues.append(&mut occ_res);
+    let mut occ_res = occurrences::process(
+        base,
+        links_result.incoming_doc_links,
+        links_result.outgoing_doc_links,
+        fix,
+    );
+    issues.append(occ_res);
     // issues.sorted()
     issues.issues()
 }
