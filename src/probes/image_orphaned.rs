1d32bd56c09cbc3381193d6b555cfb0d7eabf68e
Kevin Goslar <kevin.goslar@gmail.com>
2021-08-01 12:59:18 -0500

    Simplify iteration

--- src/probes/image_orphaned.rs
+++ src/probes/image_orphaned.rs
@@ -5,7 +5,7 @@ use super::{Issue, Issues};
 
 pub fn process(base: &Tikibase, resource_links: Vec<String>) -> Issues {
     let mut result = Issues::new();
-    for resource in base.resources.iter() {
+    for resource in &base.resources {
         let path = resource.path.to_string_lossy();
         if !resource_links.iter().any(|rl| rl == &path) {
             result.push(Box::new(OrphanedResource { path: path.into() }));
