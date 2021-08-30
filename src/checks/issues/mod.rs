0c34f572d34ff9ef14d6d15cfd469f5d9a6b2adb
Kevin Goslar <kevin.goslar@gmail.com>
2021-08-30 12:03:16 -0500

    mod.rs mod.rs

--- src/checks/issues/mod.rs
+++ src/checks/issues/mod.rs
@@ -29,3 +29,65 @@ pub(crate) use orphaned_resource::OrphanedResource;
 pub(crate) use section_without_header::SectionNoHeader;
 pub(crate) use unknown_section::UnknownSection;
 pub(crate) use unordered_sections::UnorderedSections;
+
+use crate::database::{config, Tikibase};
+use std::slice::Iter;
+
+/// an issue that was identified in the Tikibase
+pub trait Issue {
+    /// provides a human-readable description of the issue
+    fn describe(&self) -> String;
+
+    /// fixes this issue, returns a human-readable description of what it did
+    fn fix(&self, base: &mut Tikibase, config: &config::Data) -> String;
+
+    /// indicates whether this issue is fixable
+    fn fixable(&self) -> bool;
+}
+
+/// a collection of issues
+pub struct Issues(Vec<Box<dyn Issue>>);
+
+impl Issues {
+    /// appends the given issue to this issue collection
+    pub fn extend(&mut self, new_issues: Issues) {
+        self.0.extend(new_issues.0);
+    }
+
+    /// provides an iterator over borrowed references to the contained Issues
+    pub fn iter(&self) -> Iter<Box<dyn Issue>> {
+        self.0.iter()
+    }
+
+    #[cfg(test)]
+    /// indicates whether this collection contains any elements
+    pub fn is_empty(&self) -> bool {
+        self.0.is_empty()
+    }
+
+    /// provides an empty Issues instance
+    pub fn new() -> Self {
+        Issues(Vec::new())
+    }
+
+    /// adds the given Issue to this Issue collection
+    pub fn push(&mut self, issue: Box<dyn Issue>) {
+        self.0.push(issue);
+    }
+}
+
+impl Default for Issues {
+    fn default() -> Self {
+        Issues::new()
+    }
+}
+
+impl IntoIterator for Issues {
+    type Item = Box<dyn Issue>;
+
+    type IntoIter = std::vec::IntoIter<Box<dyn Issue>>;
+
+    fn into_iter(self) -> Self::IntoIter {
+        self.0.into_iter()
+    }
+}
