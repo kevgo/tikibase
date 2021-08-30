e42127f2ca832365fda5afacd06a6cf7ecfb9064
Kevin Goslar <kevin.goslar@gmail.com>
2021-08-30 09:06:02 -0500

    empty_section.rs mod.rs section_empty.rs

--- src/checks/issues/mod.rs
+++ src/checks/issues/mod.rs
@@ -1,6 +1,7 @@
 pub mod broken_image;
 pub mod broken_link;
 pub mod duplicate_section;
+pub mod empty_section;
 pub mod link_to_same_document;
 pub mod link_without_destination;
 pub mod mixed_section_capitalization;
@@ -9,6 +10,7 @@ pub mod orphaned_resource;
 pub use broken_image::BrokenImage;
 pub use broken_link::BrokenLink;
 pub use duplicate_section::DuplicateSection;
+pub use empty_section::EmptySection;
 pub use link_to_same_document::LinkToSameDocument;
 pub use link_without_destination::LinkWithoutDestination;
 pub use mixed_section_capitalization::MixCapSection;
