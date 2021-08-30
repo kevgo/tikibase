279e9656898b5de03af5eb20986c70facd55f9ec
Kevin Goslar <kevin.goslar@gmail.com>
2021-08-30 09:04:26 -0500

    duplicate_section.rs mod.rs section_duplicate.rs

--- src/checks/issues/mod.rs
+++ src/checks/issues/mod.rs
@@ -1,5 +1,6 @@
 pub mod broken_image;
 pub mod broken_link;
+pub mod duplicate_section;
 pub mod link_to_same_document;
 pub mod link_without_destination;
 pub mod mixed_section_capitalization;
@@ -7,6 +8,7 @@ pub mod orphaned_resource;
 
 pub use broken_image::BrokenImage;
 pub use broken_link::BrokenLink;
+pub use duplicate_section::DuplicateSection;
 pub use link_to_same_document::LinkToSameDocument;
 pub use link_without_destination::LinkWithoutDestination;
 pub use mixed_section_capitalization::MixCapSection;
