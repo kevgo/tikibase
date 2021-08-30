4729216159f9acad2ec8a53e759e8d589861d89e
Kevin Goslar <kevin.goslar@gmail.com>
2021-08-30 09:02:24 -0500

    mixed_section_capitalization.rs mod.rs section_capitalization.rs

--- src/checks/issues/mod.rs
+++ src/checks/issues/mod.rs
@@ -2,10 +2,12 @@ pub mod broken_image;
 pub mod broken_link;
 pub mod link_to_same_document;
 pub mod link_without_destination;
+pub mod mixed_section_capitalization;
 pub mod orphaned_resource;
 
 pub use broken_image::BrokenImage;
 pub use broken_link::BrokenLink;
 pub use link_to_same_document::LinkToSameDocument;
 pub use link_without_destination::LinkWithoutDestination;
+pub use mixed_section_capitalization::MixCapSection;
 pub use orphaned_resource::OrphanedResource;
