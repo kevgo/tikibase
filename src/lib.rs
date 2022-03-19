a4d1182d0c8d5c496373f469f64f64bc58c1e29d
Kevin Goslar <kevin.goslar@gmail.com>
2022-03-18 22:14:42 -0500

    lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -50,11 +50,11 @@ pub struct Args {
 
 #[derive(Debug, PartialEq, clap::Subcommand)]
 pub enum Command {
-    /// The command to run on CI. Finds and prints issues, does not make changes.
+    /// finds and prints issues, does not make changes
     Check,
     /// corrects all auto-fixable issues
     Fix,
-    /// Corrects all auto-fixable issues, prints all remaining issues.
+    /// corrects all auto-fixable issues, prints all remaining issues
     Pitstop,
     /// displays statistics about this Tikibase
     Stats,
