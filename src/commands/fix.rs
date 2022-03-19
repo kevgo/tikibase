24ed13c1d59d619652c4ddd50d1f09982a499fde
Kevin Goslar <kevin.goslar@gmail.com>
2022-03-19 11:33:59 -0500

    fix.rs

--- src/commands/fix.rs
+++ src/commands/fix.rs
@@ -2,14 +2,11 @@ use crate::{commands, config, fixers, Outcome, Tikibase};
 
 pub fn fix(base: &mut Tikibase, config: &config::Data) -> Outcome {
     let check_outcome = commands::check(base, config);
-    let mut fixes = vec![];
+    let mut fix_outcome = Outcome::default();
     for issue in check_outcome.issues {
         if let Some(fixed) = fixers::fix(issue, base, config) {
-            fixes.push(fixed);
+            fix_outcome.fixes.push(fixed);
         }
     }
-    Outcome {
-        issues: vec![],
-        fixes,
-    }
+    fix_outcome
 }
