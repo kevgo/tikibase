c0babcaa31a0850808d37e7ca256fb0d63be9f68
Kevin Goslar <kevin.goslar@gmail.com>
2022-03-20 20:34:41 -0500

    empty_section.rs

--- src/fixers/empty_section.rs
+++ src/fixers/empty_section.rs
@@ -5,17 +5,17 @@ use std::path::PathBuf;
 pub fn remove_empty_section(
     base: &mut Tikibase,
     section_type: String,
-    filename: PathBuf,
+    file: PathBuf,
     line: u32,
 ) -> Fix {
     let base_dir = base.dir.clone();
-    let doc = base.get_doc_mut(&filename).unwrap();
+    let doc = base.get_doc_mut(&file).unwrap();
     doc.content_sections
         .retain(|section| section.section_type() != section_type);
     doc.save(&base_dir);
     Fix::RemovedEmptySection {
         section_type,
-        filename,
+        filename: file,
         line,
     }
 }
