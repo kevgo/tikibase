6bf86db6a38bcc5d94cc4f47947487684660f1e6
Kevin Goslar <kevin.goslar@gmail.com>
2022-03-24 13:13:05 -0500

    mod.rs

--- src/fix/mod.rs
+++ src/fix/mod.rs
@@ -10,61 +10,63 @@ use crate::{Config, Issue, Location, Tikibase};
 /// fixes the given Issue
 pub fn fix(issue: Issue, base: &mut Tikibase, config: &Config) -> Option<Fix> {
     match issue {
-        Issue::BrokenImage {
-            location: _,
-            target: _,
-        } => None,
-        Issue::BrokenLink {
-            location: _,
-            target: _,
-        } => None,
-        Issue::CannotReadConfigurationFile {
-            message: _,
-            location: _,
-        } => None,
-        Issue::DuplicateSection {
-            location: _,
-            title: _,
-        } => None,
+        // actual fixes
         Issue::EmptySection { location, title } => {
             Some(empty_section::remove_empty_section(base, title, location))
         }
-        Issue::InvalidConfigurationFile {
-            message: _,
-            location: _,
-        } => None,
-        Issue::LinkToSameDocument { location: _ } => None,
-        Issue::LinkWithoutDestination { location: _ } => None,
         Issue::MissingLinks { location, links } => {
             Some(missing_links::add_occurrences(base, location, links))
         }
-        Issue::MissingFootnote {
-            location: _,
-            identifier: _,
-        } => None,
-        Issue::MixCapSection {
-            variants: _,
-            location: _,
-        } => None,
-        Issue::NoTitleSection { location: _ } => None,
         Issue::ObsoleteOccurrencesSection { location } => Some(
             obsolete_occurrences_section::remove_occurrences_section(base, location),
         ),
-        Issue::OrphanedResource { location: _ } => None,
-        Issue::SectionWithoutHeader { location: _ } => None,
-        Issue::UnclosedBacktick { location: _ } => None,
-        Issue::UnclosedFence { location: _ } => None,
-        Issue::UnknownSection {
-            location: _,
-            title: _,
-            allowed_titles: _,
-        } => None,
         Issue::UnorderedSections { location } => Some(unordered_sections::sort_sections(
             base,
             location,
             config.sections.as_ref().unwrap(),
         )),
-        Issue::UnusedFootnote {
+        // no-ops
+        Issue::BrokenImage {
+            location: _,
+            target: _,
+        }
+        | Issue::BrokenLink {
+            location: _,
+            target: _,
+        }
+        | Issue::CannotReadConfigurationFile {
+            message: _,
+            location: _,
+        }
+        | Issue::DuplicateSection {
+            location: _,
+            title: _,
+        }
+        | Issue::InvalidConfigurationFile {
+            message: _,
+            location: _,
+        }
+        | Issue::MissingFootnote {
+            location: _,
+            identifier: _,
+        }
+        | Issue::MixCapSection {
+            variants: _,
+            location: _,
+        }
+        | Issue::LinkToSameDocument { location: _ }
+        | Issue::LinkWithoutDestination { location: _ }
+        | Issue::NoTitleSection { location: _ }
+        | Issue::OrphanedResource { location: _ }
+        | Issue::SectionWithoutHeader { location: _ }
+        | Issue::UnclosedBacktick { location: _ }
+        | Issue::UnclosedFence { location: _ }
+        | Issue::UnknownSection {
+            location: _,
+            title: _,
+            allowed_titles: _,
+        }
+        | Issue::UnusedFootnote {
             location: _,
             identifier: _,
         } => None,
