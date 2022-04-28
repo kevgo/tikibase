f4f7064608eedbc4a13300af6b8bc054dc4dc579
Kevin Goslar <kevin.goslar@gmail.com>
2022-04-28 13:49:05 -0500

    dir_1.rs

--- src/check/dir_1.rs
+++ src/check/dir_1.rs
@@ -4,8 +4,8 @@ use std::path::Path;
 
 // phase 1 `Directory` check
 pub(crate) fn check_dir_1(dir: &Directory, parent: &Path, state: &mut State1) {
-    for (_filename, doc) in &dir.docs {
-        check_doc_1(doc, parent, &dir.config, state);
+    for doc in dir.docs.values() {
+        check_doc_1(doc, &dir.relative_path, &dir.config, state);
     }
     for (dirname, dir) in &dir.dirs {
         check_dir_1(dir, &parent.join(dirname), state);
