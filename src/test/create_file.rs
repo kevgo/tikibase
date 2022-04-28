10b0e6a70ff899971c918331141a45f9e9c5ebce
Kevin Goslar <kevin.goslar@gmail.com>
2022-04-28 16:26:59 -0500

    links_in_subfolder.feature create_file.rs

--- src/test/create_file.rs
+++ src/test/create_file.rs
@@ -1,8 +1,11 @@
-use std::fs::File;
+use std::fs::{self, File};
 use std::io::prelude::*;
 use std::path::Path;
 
-pub fn create_file(filename: &str, content: &str, dir: &Path) {
-    let mut file = File::create(dir.join(filename)).unwrap();
+pub fn create_file(filename: &str, content: &str, workspace: &Path) {
+    let file_path = workspace.join(filename);
+    let parent_dir = file_path.parent().unwrap();
+    fs::create_dir_all(parent_dir).unwrap();
+    let mut file = File::create(file_path).unwrap();
     file.write_all(content.as_bytes()).unwrap();
 }
