dd939aed80431f3e0311363443a5b638dd754555
Kevin Goslar <kevin.goslar@gmail.com>
2022-04-29 19:38:06 -0500

    tests

--- src/database/paths.rs
+++ src/database/paths.rs
@@ -5,3 +5,28 @@ pub fn join(path1: &str, path2: &str) -> String {
         format!("{}/{}", path1, path2)
     }
 }
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn two_paths() {
+        let have = super::join("one", "two");
+        let want = "one/two".to_string();
+        assert_eq!(have, want);
+    }
+
+    #[test]
+    fn first_path_empty() {
+        let have = super::join("", "two");
+        let want = "two".to_string();
+        assert_eq!(have, want);
+    }
+
+    #[test]
+    fn second_path_empty() {
+        let have = super::join("one", "");
+        let want = "one".to_string();
+        assert_eq!(have, want);
+    }
+}
