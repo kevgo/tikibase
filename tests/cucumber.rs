baf8c7d0e5cb433db0991500bf084b8154e09e84
Kevin Goslar <kevin.goslar@gmail.com>
2022-06-08 07:20:39 -0500

    cucumber.rs

--- tests/cucumber.rs
+++ tests/cucumber.rs
@@ -109,7 +109,7 @@ fn it_finds_no_issues(world: &mut MyWorld) {
     pretty::assert_eq!(world.output, Messages::default());
 }
 
-#[then(regex = "^the exit code is (\\d+)$")]
+#[then(expr = "the exit code is {int}")]
 fn the_exit_code_is(world: &mut MyWorld, exit_code: u8) {
     assert_eq!(world.output.exit_code, exit_code);
 }
