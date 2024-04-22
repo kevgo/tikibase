fc1bdabc2fc8e8066ce325a5e0b73ebf57586c42
Kevin Goslar <kevin.goslar@gmail.com>
2024-04-22 17:53:48 -0500

    Use impl trait syntax

--- src/test/create_file.rs
+++ src/test/create_file.rs
@@ -3,7 +3,7 @@ use fs_err::File;
 use std::io::prelude::*;
 use std::path::Path;
 
-pub fn create_file<P1: AsRef<Path>, P2: AsRef<Path>>(filename: P1, content: &str, dir: P2) {
+pub fn create_file(filename: impl AsRef<Path>, content: &str, dir: impl AsRef<Path>) {
   let filename = filename.as_ref();
   let dir = dir.as_ref();
   if let Some(parent) = filename.parent() {
