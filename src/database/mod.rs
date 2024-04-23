b7e61361c8159d0bb6deed24e109a1d55fa5c074
Kevin Goslar <kevin.goslar@gmail.com>
2024-04-22 21:44:58 -0500

    Cargo.toml mod.rs mod.rs

--- src/database/mod.rs
+++ src/database/mod.rs
@@ -1,20 +1,20 @@
 //! Read/write access to the Markdown files making up the database.
 
 mod directory;
-pub(crate) mod document;
+pub mod document;
 mod footnotes;
 mod image;
 mod line;
 mod link;
-pub(crate) mod paths;
-pub(crate) mod section;
+pub mod paths;
+pub mod section;
 mod tikibase;
 
-pub(crate) use crate::database::tikibase::Tikibase;
-pub(crate) use directory::{Directory, EntryType};
-pub(crate) use document::Document;
-pub(crate) use footnotes::{Footnote, Footnotes};
-pub(crate) use image::Image;
-pub(crate) use line::Line;
-pub(crate) use link::Link;
-pub(crate) use section::Section;
+pub use crate::database::tikibase::Tikibase;
+pub use directory::{Directory, EntryType};
+pub use document::Document;
+pub use footnotes::{Footnote, Footnotes};
+pub use image::Image;
+pub use line::Line;
+pub use link::Link;
+pub use section::Section;
